154c0e69c6765ddf0f78e16deb363d4007f17750
blitzed699 <blitzed699@gmail.com>
2026-08-05 19:13:49 +0800

    Auto-approve vision tool

--- core/safety.py
+++ core/safety.py
@@ -22,7 +22,8 @@ DESTRUCTIVE_PATTERNS = [
 DESTRUCTIVE_TOOL_NAMES = {"shell", "write_file", "run_python", "organize_files"}
 
 
-READONLY_TOOL_NAMES = {"file_read", "file_list", "open_app", "web_search"}
+READONLY_TOOL_NAMES = {"file_read", "file_list", "open_app", "web_search", "vision"}
+
 
 
 class SafetyGate:
